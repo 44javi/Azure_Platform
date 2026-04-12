c74a05cd305842b2f76959f939b081ef22c06bd3
root <root@Javi.>
2026-04-12 16:11:36 -0700

    update url to AZ-305

--- landing_zones/public/portfolio/infra/main.tf
+++ landing_zones/public/portfolio/infra/main.tf
@@ -10,17 +10,17 @@ resource "azurerm_resource_group" "main" {
 # }
 
 module "ca_portfolio" {
-  source = "../../../../modules/compute/container_apps"
+  source              = "../../../../modules/compute/container_apps"
   resource_group_name = azurerm_resource_group.main.name
-  region       = var.region
-  project      = var.project
-  environment  = var.environment
-  default_tags = local.default_tags
+  region              = var.region
+  project             = var.project
+  environment         = var.environment
+  default_tags        = local.default_tags
 
-  # log_analytics_workspace_id = data.azurerm_log_analytics_workspace.management.id
-  docker_usr                 = var.docker_usr
-  enable_custom_domain       = var.enable_custom_domain
-  custom_domain              = var.custom_domain
-  custom_domain_www          = var.custom_domain_www
-  container_image            = var.container_image
+  #log_analytics_workspace_id = data.azurerm_log_analytics_workspace.management.id
+  docker_usr           = var.docker_usr
+  enable_custom_domain = var.enable_custom_domain
+  custom_domain        = var.custom_domain
+  custom_domain_www    = var.custom_domain_www
+  container_image      = var.container_image
 }
