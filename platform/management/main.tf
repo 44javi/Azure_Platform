71c8578c00d9dd9ff57b55ec375f1263eca9f818
root <root@Javi.>
2026-05-16 11:13:37 -0700

    enable data collection rules data sources

--- platform/management/main.tf
+++ platform/management/main.tf
@@ -56,7 +56,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
           registryCollectionFrequency = 3000
           registryInfo = [{
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Group Policy\\Scripts\\Startup"
             name        = "Registry_1"
@@ -64,7 +64,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Group Policy\\Scripts\\Shutdown"
             name        = "Registry_2"
@@ -72,7 +72,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run"
             name        = "Registry_3"
@@ -80,7 +80,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Active Setup\\Installed Components"
             name        = "Registry_4"
@@ -88,7 +88,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Classes\\Directory\\ShellEx\\ContextMenuHandlers"
             name        = "Registry_5"
@@ -96,7 +96,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Classes\\Directory\\Background\\ShellEx\\ContextMenuHandlers"
             name        = "Registry_6"
@@ -104,7 +104,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Classes\\Directory\\Shellex\\CopyHookHandlers"
             name        = "Registry_7"
@@ -112,7 +112,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers"
             name        = "Registry_8"
@@ -120,7 +120,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers"
             name        = "Registry_9"
@@ -128,7 +128,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects"
             name        = "Registry_10"
@@ -136,7 +136,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects"
             name        = "Registry_11"
@@ -144,7 +144,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Internet Explorer\\Extensions"
             name        = "Registry_12"
@@ -152,7 +152,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Extensions"
             name        = "Registry_13"
@@ -160,7 +160,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Drivers32"
             name        = "Registry_14"
@@ -168,7 +168,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Windows NT\\CurrentVersion\\Drivers32"
             name        = "Registry_15"
@@ -176,7 +176,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager\\KnownDlls"
             name        = "Registry_16"
@@ -184,7 +184,7 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
             valueName   = ""
             }, {
             description = ""
-            enabled     = false
+            enabled     = true
             groupTag    = "Recommended"
             keyName     = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\Notify"
             name        = "Registry_17"
@@ -252,234 +252,3 @@ resource "azurerm_monitor_data_collection_rule" "change_tracking" {
     }
   }
 }
-
-
-#------------------------
-
-# resource "azurerm_monitor_data_collection_rule" "res-0" {
-#   data_collection_endpoint_id = ""
-#   description                 = "Data collection rule for ServiceNow."
-#   kind                        = ""
-#   location                    = var.region
-#   name                        = "dcr-servicenow-global"
-#   resource_group_name         = azurerm_resource_group.main.name
-#   tags                        = {}
-#   data_flow {
-#     built_in_transform = ""
-#     destinations       = ["Microsoft-CT-Dest"]
-#     output_stream      = ""
-#     streams            = ["Microsoft-ConfigurationChange", "Microsoft-ConfigurationChangeV2", "Microsoft-ConfigurationData"]
-#     transform_kql      = ""
-#   }
-# data_sources {
-#   extension {
-#     extension_json = jsonencode({
-#       enableFiles     = true
-#       enableInventory = true
-#       enableRegistry  = true
-#       enableServices  = true
-#       enableSoftware  = true
-#       fileSettings = {
-#         fileCollectionFrequency = 2700
-#       }
-#       inventorySettings = {
-#         inventoryCollectionFrequency = 36000
-#       }
-#       registrySettings = {
-#         registryCollectionFrequency = 3000
-#         registryInfo = [{
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Group Policy\\Scripts\\Startup"
-#           name        = "Registry_1"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Group Policy\\Scripts\\Shutdown"
-#           name        = "Registry_2"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run"
-#           name        = "Registry_3"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Active Setup\\Installed Components"
-#           name        = "Registry_4"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Classes\\Directory\\ShellEx\\ContextMenuHandlers"
-#           name        = "Registry_5"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Classes\\Directory\\Background\\ShellEx\\ContextMenuHandlers"
-#           name        = "Registry_6"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Classes\\Directory\\Shellex\\CopyHookHandlers"
-#           name        = "Registry_7"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers"
-#           name        = "Registry_8"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers"
-#           name        = "Registry_9"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects"
-#           name        = "Registry_10"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects"
-#           name        = "Registry_11"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Internet Explorer\\Extensions"
-#           name        = "Registry_12"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Internet Explorer\\Extensions"
-#           name        = "Registry_13"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Drivers32"
-#           name        = "Registry_14"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Windows NT\\CurrentVersion\\Drivers32"
-#           name        = "Registry_15"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager\\KnownDlls"
-#           name        = "Registry_16"
-#           recurse     = true
-#           valueName   = ""
-#           }, {
-#           description = ""
-#           enabled     = false
-#           groupTag    = "Recommended"
-#           keyName     = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\Notify"
-#           name        = "Registry_17"
-#           recurse     = true
-#           valueName   = ""
-#         }]
-#       }
-#       servicesSettings = {
-#         serviceCollectionFrequency = 1800
-#       }
-#       softwareSettings = {
-#         softwareCollectionFrequency = 1800
-#       }
-#     })
-#     extension_name     = "ChangeTracking-Windows"
-#     input_data_sources = []
-#     name               = "CTDataSource-Windows"
-#     streams            = ["Microsoft-ConfigurationChange", "Microsoft-ConfigurationChangeV2", "Microsoft-ConfigurationData"]
-#   }
-#   extension {
-#     extension_json = jsonencode({
-#       enableFiles     = true
-#       enableInventory = true
-#       enableRegistry  = false
-#       enableServices  = true
-#       enableSoftware  = true
-#       fileSettings = {
-#         fileCollectionFrequency = 900
-#         fileInfo = [{
-#           destinationPath       = "/etc/.*.conf"
-#           enabled               = true
-#           groupTag              = "Recommended"
-#           links                 = "Follow"
-#           maxContentsReturnable = 5000000
-#           maxOutputSize         = 500000
-#           name                  = "ChangeTrackingLinuxPath_default"
-#           pathType              = "File"
-#           recurse               = true
-#           type                  = "File"
-#           useSudo               = true
-#         }]
-#       }
-#       inventorySettings = {
-#         inventoryCollectionFrequency = 36000
-#       }
-#       servicesSettings = {
-#         serviceCollectionFrequency = 300
-#       }
-#       softwareSettings = {
-#         softwareCollectionFrequency = 300
-#       }
-#     })
-#     extension_name     = "ChangeTracking-Linux"
-#     input_data_sources = []
-#     name               = "CTDataSource-Linux"
-#     streams            = ["Microsoft-ConfigurationChange", "Microsoft-ConfigurationChangeV2", "Microsoft-ConfigurationData"]
-#   }
-# }
-#   destinations {
-#     log_analytics {
-#       name                  = "Microsoft-CT-Dest"
-#       workspace_resource_id = module.log_workspace.log_analytics_id
-#     }
-#   }
-# }
--- platform/management/outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "change_tracking_dcr_windows_id" {
-#   description = "Resource ID of the Windows Change Tracking and Inventory Data Collection Rule"
-#   value       = azurerm_monitor_data_collection_rule.change_tracking_windows.id
-# }
-
-# output "change_tracking_dcr_linux_id" {
-#   description = "Resource ID of the Linux Change Tracking and Inventory Data Collection Rule"
-#   value       = azurerm_monitor_data_collection_rule.change_tracking_linux.id
-# }
